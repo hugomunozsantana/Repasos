b697f5dcc897dcce9c4e2c58fd657697b99ed4ca
hugomunozsantana <95626271+hugomunozsantana@users.noreply.github.com>
2022-01-27 11:28:02 -0300

    Create 27-01-2022a.js

---
+++ 27-01-2022a.js
@@ -0,0 +1,8 @@
+let personaArray=["Brad","Pitt",58,"actor",["Bastardos sin gloria","El club de la pelea"]];
+
+//let carnes=[["bondiola","chorizo"["matambre","jamon"]],["pollo","alitas",["milanesa","patitas de pollo"]]];
+
+let objetoPersona={
+    dosPeliculas:[],
+};
+
